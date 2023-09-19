a6fb4d59f6ccc1965bee206922b9481f9dac57bb
Chandrabhan14 <chandrabhansinghbais14@gmail.com>
2023-09-19 13:38:56 +0530

    retro website

---
+++ script.js
@@ -0,0 +1,28 @@
+// using the gsap library & locomotive libary for js
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+// moving effect of the pages here 
+var tl = gsap.timeline()
+
+tl.to("#page1",{
+    y:"100vh",
+    scale:0.6,
+    duration:0
+})
+tl.to("#page1",{
+    y:"30vh",
+    duration:1,
+    dely:1
+
+})
+tl.to("#page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1, 
+    duration:0.8
+})
+    
